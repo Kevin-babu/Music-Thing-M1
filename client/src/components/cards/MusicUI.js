9066e93839a252eca1070cdf7c23323193462f57
Kevin Babu <bighero.0.0.7.0@gmail.com>
2026-09-07 15:24:35 +0530

    backend url update in music.js

--- client/src/components/cards/MusicUI.js
+++ client/src/components/cards/MusicUI.js
@@ -21,7 +21,7 @@ export default function MusicUI({accessToken, albumId}) {
     if (!albumId) return;
     const fetchAlbum = async () => {
       try {
-        const response = await axios.post("http://localhost:3001/api/album", {
+        const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/album`, {
             accessToken,
             albumId
         })
